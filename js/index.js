9f42ab95781186db86aaee6f0a537a049a2ce9ae
hornet-inc <dr.rc.ravindranath@gmail.com>
2025-09-03 08:44:44 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ function login() {
   const user = document.getElementById("username").value.trim();
   const pwd = document.getElementById("password").value;
 
-  if (user === "USER" && pwd === "1234") {
+  if (user === "USER" && pwd === "5855") {
     window.location.href = "user.html";
-  } else if (user === "ADMIN" && pwd === "1234") {
+  } else if (user === "ADMIN" && pwd === "5855") {
     // Redirect to blockchain auth step for admin
     window.location.href = "admin_auth.html";
   } else {
